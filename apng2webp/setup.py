73eed5a17e299d0349b36af8be750a8d3088faed
Nano Miratus <nanomiratus@gmail.com>
2016-11-30 22:21:57 +0100

    made it compatible with windows
    using pbs as alternative to sh

--- apng2webp/setup.py
+++ apng2webp/setup.py
@@ -1,6 +1,17 @@
 #!/usr/bin/env python
 
 from setuptools import setup, find_packages
+from pip.req import parse_requirements
+import pip
+import os
+
+# parse_requirements() returns generator of pip.req.InstallRequirement objects
+if os.name == 'nt':
+	install_reqs = parse_requirements('winrequirements.pip', session=pip.download.PipSession())
+else:
+	install_reqs = parse_requirements('requirements.pip', session=pip.download.PipSession())
+
+reqs = [str(ir.req) for ir in install_reqs]
 
 setup(
     name = 'apng2webp',
@@ -13,6 +24,7 @@ setup(
     description='Convert apng animations to webp animations',
     packages = find_packages(),
     scripts = ['apng2webp'],
+    install_requires = reqs,
     classifiers = [
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 2",
